6c0c20cac708617c54ca5e26bc3cb87b2c4db404
Lee Jiangwei <ljwskrskr@outlook.com>
2020-09-03 23:06:06 +0800

    add music search

--- actions.py
+++ actions.py
@@ -6,9 +6,13 @@
 
 
 from typing import Any, Text, Dict, List
+import requests
+from requests.exceptions import RequestException
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+
+BASE_URL = "http://127.0.0.1:3000"
 
 
 class SearchSongAction(Action):
@@ -18,10 +22,41 @@ class SearchSongAction(Action):
     def run(
             self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
     ) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message("action_search_song executed")
+
         song_title = tracker.get_slot("song_title")
         artist = tracker.get_slot("artist")
 
-        dispatcher.utter_message("action_search_song executed")
+        keywords = ""
+        if song_title:
+            keywords += song_title
+        if artist:
+            keywords += " " + artist
+
+        try:
+            response = requests.get(BASE_URL + "/search", params={"keywords": keywords}).json()
+
+            if 'result' not in response.keys():
+                dispatcher.utter_message("抱歉，没有找到对应的音乐")
+                return []
+
+            song_id = response['result']['songs'][0]['id']
+
+            c = requests.get(BASE_URL + "/check/music", params={"id": song_id}).json()
+
+            if not c['success']:
+                dispatcher.utter_message("抱歉，该音乐暂无版权")
+                return []
+
+            m = requests.get(BASE_URL + "/song/url", params={"id": song_id}).json()
+            song_url = m['data'][0]['url']
+
+            dispatcher.utter_message("已为您找到音乐，链接：" + song_url)
+
+        except RequestException as e:
+            print(e)
+
         print(song_title, artist)
 
         return []
